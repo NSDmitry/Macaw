024bf8cdbd716ec6481fa942d10fe13fc4bd95cb
Dmitry Serov <idmitryserov@gmail.com>
2020-08-06 20:09:17 +0300

    fixed xcode error

--- Source/model/draw/Color.swift
+++ Source/model/draw/Color.swift
@@ -60,7 +60,7 @@ open class Color: Fill {
         return rgbt( r: r, g: g, b: b, t: 0 )
     }
 
-    override func equals<T>(other: T) -> Bool where T: Color {
+    func equals(other: Color) -> Bool {
         return val == other.val
     }
 }
